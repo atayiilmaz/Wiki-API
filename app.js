2830780c076a37c3023c3bdcb9f304e39f1c74ba
Ata Berk Yılmaz <126782062+atayiilmaz@users.noreply.github.com>
2023-03-21 14:57:17 +0300

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
+
 mongoose.connect("mongodb://0.0.0.0:27017/wikiDB", {useNewUrlParser: true})
 
 const articleSchema = new mongoose.Schema({
@@ -16,20 +21,36 @@ const articleSchema = new mongoose.Schema({
 
 const Article = mongoose.model("Article", articleSchema)
 
-app.get("/articles", function (req,res) {
+
+app.route("/articles")
+  .get(function (req,res) {
     Article.find().then((foundArticles) => {
       res.send(foundArticles)
     }).catch((err) => {
       res.send(err)
     })
   })
+  .post(function (req,res) {
 
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-app.use(express.static("public"));
+      const newArticle = new Article({
+        title: req.query.title,
+        content: req.query.content
+      })
 
-//TODO
+      newArticle.save().then(() => {
+        res.send("Successfully added")
+      }).catch((err) => {
+        res.send(err)
+      })
+    })
+  .delete(function (req,res) {
+
+      Article.deleteMany().then(() => {
+        res.send("Successfully deleted")
+      }).catch((err) => {
+        res.send(err)
+      })
+    })
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
